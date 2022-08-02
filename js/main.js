1b0551b1e170394c3404d04eb597e0dbb84a6d77
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-08-02 22:42:36 +0300

    Add Header, Hero, Card sections

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let navOpenBtn = document.querySelector(".site-header__burger")
+let siteNav = document.querySelector(".site-header__navbar")
+
+navOpenBtn.addEventListener("click", ()=> {
+    siteNav.classList.toggle("site-header__navbar-on")
+})
